f1a9c74fd6638595466c076b5dba1eed0f0173e9
Jessijiaqi <148395825+Jessijiaqi@users.noreply.github.com>
2023-10-24 19:16:52 -0700

    Create W4.py

---
+++ W4.py
@@ -0,0 +1,29 @@
+def flood_fill(input_board, old, new, x, y):
+    def fill(x, y):
+        if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]) or input_board[x][y] != old:
+            return
+        input_board[x] = input_board[x][:y] + new + input_board[x][y + 1:]
+        fill(x - 1, y)  
+        fill(x + 1, y)  
+        fill(x, y - 1)  
+        fill(x, y + 1)  
+    if 0 <= x < len(input_board) and 0 <= y < len(input_board[0]) and input_board[x][y] == old:
+        fill(x, y)  
+    return input_board
+board = [
+    "......................",
+    "......##########......",
+    "......#........#......",
+    "......#........#......",
+    "......#........#####..",
+    "....###............#..",
+    "....#............###..",
+    "....##############....",
+]
+
+modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
+for row in modified_board:
+    print(row)
+modified_board = flood_fill(input_board=board, old=".", new="~", x=1, y=1)
+for row in modified_board:
+    print(row)
